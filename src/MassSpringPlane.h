14c8f96f34fd1b6d863339e136d3572a36ed15b6
Nathan-Hutton <nathan.d.hutton@gmail.com>
2025-03-31 12:42:18 -0600

    Got rid of a struct I wasn't using anymore

--- src/MassSpringPlane.h
+++ src/MassSpringPlane.h
@@ -6,15 +6,6 @@
 #include <chrono>
 #include "Physics.h"
 
-struct pair_hash {
-    template <typename T1, typename T2>
-    std::size_t operator()(const std::pair<T1, T2>& p) const {
-        auto h1 = std::hash<T1>{}(p.first);
-        auto h2 = std::hash<T2>{}(p.second);
-        return h1 ^ (h2 << 1);
-    }
-};
-
 struct TripletInfo {
     int row;
     int col;
